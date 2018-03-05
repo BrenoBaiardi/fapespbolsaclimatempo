7fee290bed187e8e97e123848f3df4bc802f6285
Baiardi <brenocbo@gmail.com>
2018-03-04 23:39:35 -0300

    melhoria na classe Scrape, e comentários

--- Plot.py
+++ Plot.py
@@ -3,14 +3,16 @@ import matplotlib.dates as mdates
 from datetime import datetime
 
 class Plot():           
-    def plotGraph(self,arr,label):
+    def plotGraph(self,arr,label): #recebe uma lista a ser plotada e uma label de identificação
         res = {"time":[],"value":[]}
         for i in arr:
             res["time"].append(datetime.strptime(i[0], "%d/%m/%Y %H:%M:%S"))
             res["value"].append(i[1])
-        dates = mdates.date2num(res["time"])
+        # os comandos seguintes garantes que os
+        # valores contidos no eixo x possam existir
+        # e ser exibidos de maneira legível
+        #dates = mdates.date2num(res["time"])
         plt.plot(res["time"], res["value"],label=label)
-        plt.legend
         return (plt.gcf().autofmt_xdate())
                 
     def showGraph(self):
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from Scraper import Scraper
-from Plot import Plot
-from MongoManage import MongoManage
+from Scraper import Scraper         # O projeto utiliza classes
+from Plot import Plot               # próprias para cada função requerida
+from MongoManage import MongoManage # que devem estar no mesmo diretório
 
+# Recebendo dados do url contido na classe Scraper
 scraper = Scraper()
-arr_guap,arr_colider = scraper.getData()
-mongo = MongoManage()
+arr_guap = scraper.getData("http://mapas-hidro.ana.gov.br/Usuario/DadoPesquisar.aspx?est=150358540&dado=Chuva&fim=636554214000000000")
+arr_colider = scraper.getData("http://mapas-hidro.ana.gov.br/Usuario/DadoPesquisar.aspx?est=105755460&dado=Chuva&fim=636553728000000000")
+
 #popula o banco com os dados coletados pelo scraper
+mongo = MongoManage()
 mongo.populateCollection(arr_guap,"GUAPORÉ")
 mongo.populateCollection(arr_colider,"COLIDER")
 
