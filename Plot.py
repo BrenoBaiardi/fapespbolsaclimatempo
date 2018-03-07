35c82292fbb2a47e350096d3c336d73d9c79e99f
Breno Baiardi <brenocbo@gmail.com>
2018-03-07 11:30:18 -0300

    push final com instruções

--- Plot.py
+++ Plot.py
@@ -3,16 +3,14 @@ import matplotlib.dates as mdates
 from datetime import datetime
 
 class Plot():           
-    def plotGraph(self,arr,label): #recebe uma lista a ser plotada e uma label de identificação
+    def plotGraph(self,arr,label):
         res = {"time":[],"value":[]}
         for i in arr:
             res["time"].append(datetime.strptime(i[0], "%d/%m/%Y %H:%M:%S"))
             res["value"].append(i[1])
-        # os comandos seguintes garantes que os
-        # valores contidos no eixo x possam existir
-        # e ser exibidos de maneira legível
-        #dates = mdates.date2num(res["time"])
+        dates = mdates.date2num(res["time"])
         plt.plot(res["time"], res["value"],label=label)
+        plt.legend
         return (plt.gcf().autofmt_xdate())
                 
     def showGraph(self):
--- Scraper.py
+++ Scraper.py
@@ -2,25 +2,40 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 class Scraper:
-    def getData(self,link):
-        print("Coletando dados...")
-        soup = BeautifulSoup(urllib.request.urlopen(link).read(), "html.parser")
-        table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="grd")
-        rows = table.findAll(lambda tag: tag.name=='tr')
-        #remove a primeira linha correspondente ao título das colunas
-        rows.pop(0)
-        values=[]
-        for row in rows:
-            aux=[]
-            cells = row.findChildren('td')
-            if len(cells)==4:
-                aux.append(cells[-2].getText())
-                aux.append(cells[-1].getText())
-                values.append(aux)
-        print("Dados coletados")
-        return values
+   
+   def getData(self):
+      print("Coletando dados...")
+      soup = BeautifulSoup(urllib.request.urlopen('http://mapas-hidro.ana.gov.br/Usuario/DadoPesquisar.aspx?est=150358540&dado=Chuva&fim=636554214000000000').read(), "html.parser")
+      table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="grd") 
+      rows = table.findAll(lambda tag: tag.name=='tr')
+      #remove a primeira linha correspondente ao título das colunas
+      rows.pop(0)
+      values_guap=[]
+      for row in rows:
+         aux=[]
+         cells = row.findChildren('td')
+         if len(cells)==4:
+             aux.append(cells[-2].getText())
+             aux.append(cells[-1].getText())
+             values_guap.append(aux)
 
-########## PROBLEMA ##############
+      #UHE COLIDER METEOROLÓGICA
+      soup = BeautifulSoup(urllib.request.urlopen("http://mapas-hidro.ana.gov.br/Usuario/DadoPesquisar.aspx?est=105755460&dado=Chuva&fim=636553728000000000").read(), "html.parser")
+      table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="grd") 
+      rows = table.findAll(lambda tag: tag.name=='tr')
+      rows.pop(0)
+      values_colider=[]
+      for row in rows:
+         aux=[]
+         cells = row.findChildren('td')
+         if len(cells)==4:
+             aux.append(cells[-2].getText())
+             aux.append(cells[-1].getText())
+             values_colider.append(aux)
+      print("Dados coletados")
+      return values_guap,values_colider
+
+
 #o site da um link diferente pra cada dia em que o acesso é realizado
 #então o programa não está pegando dados em tempo real
 #e sim pegando todos 4 dias antes do dia 28, (acessado 28/01)
